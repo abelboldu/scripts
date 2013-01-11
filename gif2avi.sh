4cf1004d92d9529e53b96e60625a773a494ad02d
Abel Boldú <abel.boldu@gmx.com>
2013-01-11 12:49:00 +0100

    gif transparecy fix

--- gif2avi.sh
+++ gif2avi.sh
@@ -4,6 +4,6 @@
 if [ -z $1 ];
 then echo "Missing param!"; exit;
 fi
-convert -verbose -quality 90 $1 temp%08d.png
-ffmpeg   -i temp%08d.png -vcodec mpeg4  -b 5000k "$1.avi"
+convert -verbose -coalesce -background black  -quality 100 $1 temp%08d.png
+ffmpeg   -i temp%08d.png -vcodec mpeg4  -vb 20M "$1.avi"
 rm temp*.png
